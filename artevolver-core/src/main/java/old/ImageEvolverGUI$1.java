1c7165764d61086bd006018cfa36b75e0132ddd8
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2021-01-15 16:25:18 -0300

    Converted to modular LibGDX maven project/application.

---
+++ artevolver-core/src/main/java/old/ImageEvolverGUI$1.java
@@ -0,0 +1,7 @@
+package old;
+
+final class ImageEvolverGUI$1 implements Runnable {
+	public void run() {
+		new ImageEvolverGUI("Image Evolver ");
+	}
+}
---
+++ artevolver-desktop/src/main/java/com/rndmodgames/artevolver/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.rndmodgames.artevolver.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rndmodgames.artevolver.ArtEvolver;
+
+public class DesktopLauncher {
+    
+	public static void main (String[] arg) {
+	    
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArtEvolver(), config);
+		
+	}
+}

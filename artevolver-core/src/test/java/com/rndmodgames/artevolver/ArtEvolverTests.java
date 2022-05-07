979346be319f1a285e6ad8d9cad8c3ca9992e066
Nicolas Alvargonzalez <n.alvargonzalez@gmail.com>
2022-05-07 18:22:45 -0300

    Add a basic unit test.

---
+++ artevolver-core/src/test/java/com/rndmodgames/artevolver/ArtEvolverTests.java
@@ -0,0 +1,15 @@
+package com.rndmodgames.artevolver;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ArtEvolverTests {
+
+    @Test
+    void basicArtEvolverTest() {
+        
+        //
+        assertEquals(1, 1);
+    }
+}
